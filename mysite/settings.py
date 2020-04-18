5851188849920b15460ef98e1c3457ecbe48cda4
isaac1750 <bbonsu1750@gmail.com>
2020-04-18 11:35:53 -0400

    update

--- mysite/settings.py
+++ mysite/settings.py
@@ -133,6 +133,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
 
